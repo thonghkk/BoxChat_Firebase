9c029d97b4d7bcd924cfaadc14d0fda1ed8715ff
thonghkk <nguyenvanthonghkk@gmail.com>
2021-04-21 23:48:01 +0700

    nothing

--- app/src/main/java/com/example/boxchat/ui/main/setting/InfoAppActivity.kt
+++ app/src/main/java/com/example/boxchat/ui/main/setting/InfoAppActivity.kt
@@ -10,18 +10,12 @@ import com.example.boxchat.base.BaseActivity
 
 class InfoAppActivity : BaseActivity() {
     private lateinit var mImgBackSetting: ImageView
-    private lateinit var mNumberUser:TextView
     private lateinit var mSettingViewModel: SettingViewModel
 
     override fun getLayoutID() = R.layout.activity_info_app
     override fun onCreateActivity(savedInstanceState: Bundle?) {
         mImgBackSetting = findViewById(R.id.mImgBackSetting)
         mSettingViewModel = ViewModelProvider(this).get(SettingViewModel::class.java)
-        mNumberUser = findViewById(R.id.mNumberUser)
-
-        mSettingViewModel.userList.observe(this, Observer { users->
-            mNumberUser.text = "The total number of users is currently ${users.size}"
-        })
 
         mImgBackSetting.setOnClickListener {
             onBackPressed()
